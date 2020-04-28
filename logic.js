aa05042b8352412b31c495e82b81b8f66e9e1f62
Stuhl <BasicsDone7@web.de>
2020-04-28 21:23:56 +0200

    - added albums songs - added functionality to show albums songs if picture is clicked

--- logic.js
+++ logic.js
@@ -4,6 +4,82 @@ document.addEventListener("DOMContentLoaded", () => {
   const main = document.querySelector("main")
   const footer = document.querySelector("footer")
   const drawerButton = document.querySelector("#drawer-button")
+  const music = document.querySelector("#music-layout > #content")
+  const musicContainer = document.querySelector("#music-layout")
+
+  const albumSongs = {
+    "0": [
+      "All We Know",
+      "Pressure",
+      "Emergency",
+      "Brighter",
+      "Here We Go Again",
+      "Never Let This Go",
+      "Whoa",
+      "Conspiracy",
+      "Franklin",
+      "My Heart"
+    ],
+    "1": [
+      "For a Pessimist, I'm Pretty Optimistic",
+      "Thats What You Get",
+      "Hallelujah",
+      "Misery Business",
+      "When It Rains",
+      "Let The Flames Begin",
+      "Miracle",
+      "Crushcrushcrush",
+      "We Are Broken",
+      "Fences",
+      "Born For This"
+    ],
+    "2": [
+      "Careful",
+      "Ignorance",
+      "Playing God",
+      "Brick by Boring Brick",
+      "Turn It Off",
+      "The Only Exception",
+      "Feeling Sorry",
+      "Looking Up",
+      "Where the Lines Overlap",
+      "Misguided Ghosts",
+      "All I Wanted"
+    ],
+    "3": [
+      "Fast In My Car",
+      "Now",
+      "Grow Up",
+      "Daydreaming",
+      "Interlude: Moving On",
+      "Ain't It Fun",
+      "Part II",
+      "Last Hope",
+      "Still Into You",
+      "Anklebiters",
+      "Interlude: Holiday",
+      "Proof",
+      "Hate To See Your Heart Break",
+      "(One of Those) Crazy Girls",
+      "Interlude: I'm Not Angry Anymore",
+      "Be Alone",
+      "Future"
+    ],
+    "4": [
+      "Hard Times",
+      "Rose-Colored Boy",
+      "Told You So",
+      "Forgiveness",
+      "Fake Happy",
+      "26",
+      "Pool",
+      "Grudges",
+      "Caught in the Middle",
+      "Idle Worship",
+      "No Friend",
+      "Tell me How"
+    ]
+  }
 
   function resetTransition(arrayOfElements) {
     for (let element of arrayOfElements) {
@@ -51,5 +127,45 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     })
   })
+
+  // music element
+  function attachEvents() {
+    const images = music.getElementsByTagName("img")
+
+    for (let img of images) {
+      const keyValue = img.attributes["0"].value
+      const name = img.attributes["1"].value
+      const thisAlbumSongs = albumSongs[keyValue]
+
+      img.addEventListener("click", () => {
+        const template = albumTemplate(thisAlbumSongs, name)
+        musicContainer.insertAdjacentHTML("beforeend", template)
+        musicContainer.lastElementChild.scrollIntoView()
+      })
+    }
+  }
+
+  function albumTemplate(songs, name) {
+    const ol = getList(songs)
+    return `
+    <details open id="album-layout">
+      <summary>${name}</summary>
+      ${ol}
+    </details>
+    `
+  }
+
+  function getList(songs) {
+    let ul = ``
+    songs.forEach(item => {
+      ul += `<li>${item}</li>`
+    })
+    return `
+    <ol>
+      ${ul}
+    </ol>
+    `
+  }
+  attachEvents()
 
 })
